151a5bc245419b653f071f0f395151b770e5714a
isha <ishaa9856@gmail.com>
2022-05-08 12:30:11 +0530

    fibonacci

---
+++ FibonacciNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FibonacciNumber {
+
+  public static int fib(int n) {
+	  if(n==0|| n==1) {
+		  return n;
+	  }
+	  return fib(n-1)+fib(n-2);
+  }
+  public static void main(String [] args) {
+      System.out.println("Enter the required position of number: ");
+      Scanner sc=new Scanner(System.in);
+	  int n=sc.nextInt();
+      
+	  System.out.println("The number on "+n+ " position : " +fib(n));
+  }
+}
