42790ce974a98ec202397fbbd48c4d3d27ca597d
Isha <101055449+ishaa100@users.noreply.github.com>
2023-08-01 21:41:57 +0530

    Add files via upload

---
+++ Priority_queue.java
@@ -0,0 +1,30 @@
+  
+
+import java.util.*;
+
+public class Priority_queue {
+
+
+	public static void main(String args[]){
+		
+		Queue<Integer> pQueue
+			= new PriorityQueue<Integer>();
+
+		
+		pQueue.add(10);
+		pQueue.add(2);
+		pQueue.add(15);
+
+		
+		System.out.println(pQueue.peek());
+
+		
+		System.out.println(pQueue.poll());
+
+		
+		System.out.println(pQueue.peek());
+	}
+}
+
+    
+
