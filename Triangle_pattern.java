9fb9e013368235324b2e55263e2bcdf922a1f5dc
Isha <101055449+ishaa100@users.noreply.github.com>
2023-09-23 21:47:27 +0530

    Add files via upload

---
+++ Triangle_pattern.java
@@ -0,0 +1,34 @@
+
+    import java.io.*;
+
+
+public class Triangle_pattern {
+	
+	public static void StarRightTriangle(int n)
+	{
+		int a, b;
+
+		
+		for (a = 0; a < n; a++) {
+
+			
+			for (b = 0; b <= a; b++) {
+				
+				System.out.print("* ");
+			}
+
+			
+			System.out.println();
+		}
+	}
+
+	
+	public static void main(String args[])
+	{
+		int k = 5;
+		StarRightTriangle(k);
+	}
+}
+
+    
+
